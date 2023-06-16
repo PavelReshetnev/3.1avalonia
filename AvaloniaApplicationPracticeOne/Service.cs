d06eeacbbc8b259a21889cd80afd8fdce89c09a6
PavelReshetnev <pasha8804088@gmail.com>
2023-06-16 18:31:11 +0700

    Initial commit

---
+++ AvaloniaApplicationPracticeOne/Service.cs
@@ -0,0 +1,20 @@
+﻿using AvaloniaApplicationPracticeOne.Models;
+
+namespace AvaloniaApplicationPracticeOne;
+
+public class Service
+{
+    // Переменная хранит экземпляр контекста
+    private static PostgresContext? _db;
+
+    // Метод, если экземпляр еще не создан, создает и возвращает его
+    // Если экземпляр создан, возвращает его
+    public static PostgresContext  GetDbContext()
+    {
+        if (_db == null)
+        {
+            _db = new PostgresContext();
+        }
+        return _db;
+    }
+}
